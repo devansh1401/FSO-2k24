a8e89039ea91fabe0ce4741feec5a94e843000b8
Devansh Kukreja <devansh.mmis@gmail.com>
2024-02-04 14:05:09 +0530

    exercise 1.1 done

---
+++ part-1/courseinfo/src/App.jsx
@@ -0,0 +1,39 @@
+function Header({course}){
+  return <h1>{course}</h1>;
+}
+
+function Content({parts , exercises}){
+  return (
+    <p>
+        {parts} {exercises}
+      </p>
+  );
+}
+
+function Total({exercises}){
+  return(
+    <p>Number of exercises {exercises}</p>
+  );
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content parts={part1} exercises={exercises1} />
+      <Content parts={part2} exercises={exercises2} />
+      <Content parts={part3} exercises={exercises3} />
+      <Total exercises={exercises1+exercises2+exercises3}/>
+    </div>
+  )
+}
+
+export default App
+
