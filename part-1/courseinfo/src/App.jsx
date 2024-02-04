63c457028bbf6605799e5557773abcf6fc6bea23
Devansh Kukreja <devansh.mmis@gmail.com>
2024-02-04 14:32:08 +0530

    part 1.2

--- part-1/courseinfo/src/App.jsx
+++ part-1/courseinfo/src/App.jsx
@@ -1,12 +1,22 @@
+function Part({ name, exercises }) {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+}
+
 function Header({course}){
   return <h1>{course}</h1>;
 }
 
-function Content({parts , exercises}){
+function Content({ part1, exercises1, part2, exercises2, part3, exercises3 }) {
   return (
-    <p>
-        {parts} {exercises}
-      </p>
+    <div>
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
+    </div>
   );
 }
 
@@ -27,9 +37,14 @@ const App = () => {
   return (
     <div>
       <Header course = {course}/>
-      <Content parts={part1} exercises={exercises1} />
-      <Content parts={part2} exercises={exercises2} />
-      <Content parts={part3} exercises={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total exercises={exercises1+exercises2+exercises3}/>
     </div>
   )
@@ -37,3 +52,51 @@ const App = () => {
 
 export default App
 
+                   // By maps in JS 
+
+
+// function Part({ name, exercises }) {
+//   return (
+//     <p>
+//       {name} {exercises}
+//     </p>
+//   );
+// }
+
+// function Header({course}){
+//   return <h1>{course}</h1>;
+// }
+
+// function Total({exercises}){
+//     return(
+//       <p>Number of exercises {exercises}</p>
+//     );
+//     }
+// function Content({ parts }) {
+//   return (
+//     <div>
+//       {parts.map((part, index) => (
+//         <Part key={index} name={part.name} exercises={part.exercises} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// const App = () => {
+//   const course = 'Half Stack application development';
+//   const parts = [
+//     { name: 'Fundamentals of React', exercises: 10 },
+//     { name: 'Using props to pass data', exercises: 7 },
+//     { name: 'State of a component', exercises: 14 },
+//   ];
+
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content parts={parts} />
+//       <Total exercises={parts.reduce((sum, part) => sum + part.exercises, 0)} />
+//     </div>
+//   );
+// };
+
+// export default App;
